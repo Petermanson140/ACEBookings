9f420fd1f72d05139454eeb0cbd883a67949ae83
Peter Manson <120554793+Petermanson140@users.noreply.github.com>
2023-12-10 12:56:41 +0000

    Add files via upload

---
+++ CourtSurfacesrepository.java
@@ -0,0 +1,9 @@
+package com.example.myapp.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourtSurfacesrepository extends JpaRepository<CourtSurfaces, Integer> {
+    // You can add custom query methods here if needed
+}
+
+
