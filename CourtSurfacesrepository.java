ec36aba8c7fb31db3a44ff3990d364faa01bb66c
Peter Manson <120554793+Petermanson140@users.noreply.github.com>
2023-12-11 10:26:35 +0000

    Delete CourtSurfacesrepository.java

--- CourtSurfacesrepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapp.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourtSurfacesrepository extends JpaRepository<CourtSurfaces, Integer> {
-    // You can add custom query methods here if needed
-}
-
-
